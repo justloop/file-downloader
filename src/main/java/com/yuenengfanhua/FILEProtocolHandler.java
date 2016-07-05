6b095637beb3445f92d6ad4a58820aba4beee3bc
gejun <jun.ge@starhub.com>
2016-07-05 17:00:14 +0800

    remove file

---
+++ src/main/java/com/yuenengfanhua/FILEProtocolHandler.java
@@ -0,0 +1,43 @@
+package com.yuenengfanhua;
+
+import com.yuenengfanhua.FileInfo;
+import com.yuenengfanhua.protocolhandler.AbstractProtocolHandler;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.stereotype.Component;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+/**
+ * Created by gejun on 1/7/16.
+ */
+@Component
+public class FILEProtocolHandler extends AbstractProtocolHandler {
+    @Override
+    public String getName() {
+        return "FileProtocolHandler";
+    }
+
+    @Override
+    public void process(FileInfo file) throws MalformedURLException, IOException {
+        FileInputStream in = null;
+        FileOutputStream fout = null;
+        try
+        {
+            in  = new FileInputStream(file.getUrl());
+            fout = new FileOutputStream(downloadDir+ FilenameUtils.getName(file.getUrl()));
+            super.process(in,fout,file);
+        }
+        finally {
+            if (in != null) {
+                in.close();
+            }
+            if (fout != null) {
+                fout.flush();
+            }
+        }
+    }
+
+}
